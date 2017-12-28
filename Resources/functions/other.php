e7088c9c9d70253c579591da3afaea410b5a61f4
moresir <zhangzeqiang@eelly.net>
2017-12-28 17:39:30 +0800

    添加获取毫秒时间戳方法

--- Resources/functions/other.php
+++ Resources/functions/other.php
@@ -274,3 +274,15 @@ if (!function_exists('mobileHide')) {
             substr($mobile, -2) : $mobile;
     }
 }
+
+if (!function_exists('getMillisecond')) {
+    /**
+     * 获取毫秒时间戳.
+     *
+     * @return string
+     */
+    function getMillisecond() {
+        list($s1, $s2) = explode(' ', microtime());
+        return sprintf('%.0f', (floatval($s1) + floatval($s2)) * 1000);
+    }
+}
